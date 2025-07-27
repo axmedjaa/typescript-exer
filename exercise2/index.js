90e897638a84cbab130b8a6e11ab3f5077ee4dcf
axmedjaa <axmedja44@gmail.com>
2025-07-27 10:49:50 +0300

    typescript exercise2

---
+++ exercise2/index.js
@@ -0,0 +1,14 @@
+// 1
+var names = ["ahmed", "ali", "xamdi"];
+var grades = [12, 78, 98, 68];
+var statuses = [false, true];
+names.push(12);
+grades.push(false);
+statuses.push("hi");
+// 2
+var products = ["Phone", "Laptop"];
+// 3
+// correct orders
+var location1 = ["xamar", 100, 200];
+// wrong orders
+var location2 = [12, "xamar", false];
---
+++ exercise2/index.ts
@@ -0,0 +1,15 @@
+// 1
+let names:string[]=["ahmed","ali","xamdi"]
+let grades:number[]=[12,78,98,68]
+let statuses:boolean[]=[false,true]
+// wrong asign
+// names.push(12)
+// grades.push(false)
+// statuses.push("hi")
+// 2
+let products:string[] = ["Phone", "Laptop"];
+// 3
+// correct orders
+let location1:[string,number,number]=["xamar",100,200]
+// wrong orders
+// let location2:[string,number,number]=[12,"xamar",false]
